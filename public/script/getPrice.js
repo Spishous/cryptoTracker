eca17155d02f7910e15ee1d25b77e711ef7dda47
Rayan Lucas <rlucas974@outlook.fr>
2022-04-27 23:45:51 +0200

    remove promise

--- public/script/getPrice.js
+++ public/script/getPrice.js
@@ -1,29 +1,16 @@
 var currentQuote=1,currentId=0;
 
-async function getLatestQuote(currentId2=currentId){
-    return new Promise(function (resolve, reject) {
+async function getLatestQuote(currentId2=currentId,callback){
         let xhr = new XMLHttpRequest();
         xhr.open("GET", "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id="+currentId2);
         xhr.setRequestHeader("X-CMC_PRO_API_KEY", "b3163f42-6606-4471-8647-da8d1adb922d");
 
         xhr.onload = function () {
             if (this.status >= 200 && this.status < 300) {
-                resolve(JSON.parse(xhr.response)['data'][currentId2]['quote']['USD']['price']);
-            } else {
-                reject({
-                    status: this.status,
-                    statusText: xhr.statusText
-                });
+                callback(JSON.parse(xhr.response)['data'][currentId2]['quote']['USD']['price']);
             }
-        };
-        xhr.onerror = function () {
-            reject({
-                status: this.status,
-                statusText: xhr.statusText
-            });
-        };
+        }
         xhr.send();
-    })
 }
 function get2Decimal(float){
     let result=parseFloat(float.toString().slice(0,float.toString().indexOf(".")+3));
