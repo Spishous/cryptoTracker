4278a711976ee7744c7506c306a1977d81f6642f
Rayan Lucas <rlucas974@outlook.fr>
2022-04-28 02:17:02 +0200

    content-type correction

--- public/script/getPrice.js
+++ public/script/getPrice.js
@@ -1,13 +1,14 @@
 var currentQuote=1,currentId=0,url="/crypto-api/";
 aze=document.querySelector('.qsdqsd');
 function getLatestQuote(currentId2=currentId,callback){
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", url+currentId2);
-        xhr.onload = ()=>{
-            let result=JSON.parse(xhr.response)[currentId2]['quote']['USD']['price'];
-            callback(result);
-        }
-        xhr.send();
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url+currentId2);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onload = ()=>{
+        let result=xhr.response[currentId2]['quote']['USD']['price'];
+        callback(result);
+    }
+    xhr.send();
 }
 function getToDecimal(float){
     let result=parseFloat(float.toString().slice(0,float.toString().indexOf(".")+3));
